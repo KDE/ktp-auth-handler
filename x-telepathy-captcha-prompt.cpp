40dc33b48ca1478faf38280d76e883bd55e92d61
Jeremy Whiting <jpwhiting@kde.org>
2012-03-05 15:53:57 -0700

    Add Telepathy Captcha Prompt dialog.

---
+++ x-telepathy-captcha-prompt.cpp
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2012 Jeremy Whiting <jpwhiting>kde.org>
+ * Copyright (C) 2011 David Edmundson <kde@davidedmundson.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "x-telepathy-captcha-prompt.h"
+#include "ui_x-telepathy-captcha-prompt.h"
+
+#include <TelepathyQt/CaptchaAuthentication>
+#include <TelepathyQt/Captcha>
+#include <TelepathyQt/PendingCaptchas>
+#include <TelepathyQt/PendingReady>
+
+#include <KIcon>
+#include <KDebug>
+
+XTelepathyCaptchaPrompt::XTelepathyCaptchaPrompt(const Tp::ChannelPtr &channel, QWidget *parent)
+    : KDialog(parent),
+      ui(new Ui::XTelepathyCaptchaPrompt),
+      m_channel(channel)
+{
+    ui->setupUi(mainWidget());
+
+    setWindowIcon(KIcon(QLatin1String("telepathy-kde")));
+    setButtons(KDialog::Ok | KDialog::Cancel | KDialog::User1);
+    setButtonText(KDialog::User1, i18n("Reload"));
+
+    ui->accountIcon->setPixmap(KIcon(QLatin1String("dialog-password")).pixmap(60, 60));
+
+    connect(m_channel->becomeReady(Tp::Channel::FeatureCaptcha), SIGNAL(finished(Tp::PendingOperation *)),
+            this, SLOT(onCaptchaAuthFinished(Tp::PendingOperation*)));
+}
+
+XTelepathyCaptchaPrompt::~XTelepathyCaptchaPrompt()
+{
+    delete ui;
+}
+
+void XTelepathyCaptchaPrompt::onRequestCaptchasFinished(Tp::PendingOperation *operation)
+{
+    if (operation->isError()) {
+        qWarning() << "Captchas Finished error: " << operation->errorMessage() << operation->errorName();
+        return;
+    }
+
+    Tp::PendingCaptchas *captchas = qobject_cast<Tp::PendingCaptchas*>(operation);
+    Tp::Captcha captcha = captchas->captcha();
+
+    if (!captcha.label().isEmpty())
+        ui->title->setText(captcha.label());
+    else
+        if (captcha.type() == Tp::CaptchaAuthentication::OCRChallenge)
+            ui->title->setText(i18n("Enter the text below"));
+
+    QPixmap pixmap;
+    pixmap.loadFromData(captcha.data());
+    pixmap = pixmap.scaledToHeight(100, Qt::SmoothTransformation);
+    ui->imageLabel->setPixmap(pixmap);
+
+    m_currentCaptchaId = captcha.id();
+
+    ui->captchaLineEdit->clear();
+
+    // Enable the buttons.
+}
+
+void XTelepathyCaptchaPrompt::onCaptchaAuthFinished(Tp::PendingOperation *operation)
+{
+    if (operation->isError()) {
+        qWarning() << "Captchas feature ready error: " << operation->errorMessage() << operation->errorName();
+        return;
+    }
+
+    requestCaptcha();
+
+    KDialog::show();
+}
+
+void XTelepathyCaptchaPrompt::requestCaptcha()
+{
+    Tp::CaptchaAuthenticationPtr captchaAuth = m_channel->captchaAuthentication();
+    connect(captchaAuth->requestCaptchas(), SIGNAL(finished(Tp::PendingOperation*)),
+            this, SLOT(onRequestCaptchasFinished(Tp::PendingOperation*)));
+}
+
+void XTelepathyCaptchaPrompt::onAnswerFinished(Tp::PendingOperation *operation)
+{
+    if (operation->isError()) {
+        qWarning() << "Answer error" << operation->errorMessage() << operation->errorName();
+        // Reload the captcha since we got it wrong (probably).
+        requestCaptcha();
+        return;
+    }
+
+    // Not an error, so close the dialog/window.
+    connect(m_channel->requestClose(), SIGNAL(finished(Tp::PendingOperation*)),
+            SLOT(deleteLater()));
+    accept();
+}
+
+void XTelepathyCaptchaPrompt::onCancelFinished(Tp::PendingOperation *operation)
+{
+    connect(m_channel->requestClose(), SIGNAL(finished(Tp::PendingOperation*)),
+            SLOT(deleteLater()));
+    reject();
+}
+
+void XTelepathyCaptchaPrompt::slotButtonClicked(int button)
+{
+    switch (button)
+    {
+    case KDialog::Ok:
+        {
+            QString answer = ui->captchaLineEdit->text();
+            Tp::CaptchaAuthenticationPtr captchaAuth = m_channel.data()->captchaAuthentication();
+            connect(captchaAuth->answer(m_currentCaptchaId, answer), SIGNAL(finished(Tp::PendingOperation*)),
+                    SLOT(onAnswerFinished(Tp::PendingOperation*)));
+        }
+        break;
+    case KDialog::Cancel:
+        {
+            Tp::CaptchaAuthenticationPtr captchaAuth = m_channel.data()->captchaAuthentication();
+            connect(captchaAuth->cancel(Tp::CaptchaCancelReasonUserCancelled), SIGNAL(finished(Tp::PendingOperation*)),
+                    SLOT(onCancelFinished(Tp::PendingOperation*)));
+        }
+        break;
+    case KDialog::User1:
+        {
+            requestCaptcha();
+        }
+        break;
+    }
+}
+
+#include "x-telepathy-captcha-prompt.moc"
---
+++ x-telepathy-captcha-prompt.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2012 Jeremy Whiting <jpwhiting>kde.org>
+ * Copyright (C) 2011 David Edmundson <kde@davidedmundson.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef CAPTCHAPROMPT_H
+#define CAPTCHAPROMPT_H
+
+#include <KDialog>
+
+#include <TelepathyQt/Account>
+
+namespace Ui
+{
+    class XTelepathyCaptchaPrompt;
+}
+
+class XTelepathyCaptchaPrompt : public KDialog
+{
+    Q_OBJECT
+
+public:
+    explicit XTelepathyCaptchaPrompt(const Tp::ChannelPtr &channel, QWidget *parent = 0);
+    ~XTelepathyCaptchaPrompt();
+
+protected Q_SLOTS:
+    virtual void slotButtonClicked(int button);
+
+private Q_SLOTS:
+    void onRequestCaptchasFinished(Tp::PendingOperation *operation);
+    void onCaptchaAuthFinished(Tp::PendingOperation *operation);
+    void onAnswerFinished(Tp::PendingOperation *operation);
+    void onCancelFinished(Tp::PendingOperation *operation);
+
+private:
+    void requestCaptcha();
+
+    Ui::XTelepathyCaptchaPrompt *ui;
+    Tp::ChannelPtr m_channel;
+    unsigned int m_currentCaptchaId;
+};
+
+#endif // PASSWORDPROMPT_H
